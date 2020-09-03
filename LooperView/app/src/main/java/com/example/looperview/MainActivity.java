c691a8500a4271b079af6f2e4778f45acbfd8c42
ChienyehLin <linchienyeh_jaden@outlook.com>
2020-09-03 18:00:04 +0800

    Android Custom View LooperView

--- LooperView/app/src/main/java/com/example/looperview/MainActivity.java
+++ LooperView/app/src/main/java/com/example/looperview/MainActivity.java
@@ -1,44 +1,56 @@
 package com.example.looperview;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.VideoView;
+import android.widget.Toast;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class MainActivity extends Activity {
     OpeningLooperView mOpeningLooperView;
+    Button mButtonTry;
     ArrayList<ImageView> mList;
-
+    ArrayList<String> mMainTitle;
+    ArrayList<String> mSubTitle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mOpeningLooperView = findViewById(R.id.opening_looper_view);
         initData();
+        initView();
 
+    }
 
-        mOpeningLooperView.setData(new OpeningLooperView.InnerPageAdapter(mList) {
-            @Override
-            public int getDataSize() {
-                return mList.size();
-            }
-
+    private void initView() {
+        mOpeningLooperView = findViewById(R.id.opening_looper_view);
+        mOpeningLooperView.setData(new OpeningLooperView.InnerPageAdapter(mList.size()) {
             @Override
             protected View getItemView(ViewGroup container, int itemPosition) {
-                return null;
+                return mList.get(itemPosition);
             }
         }, new OpeningLooperView.TitleBindListener() {
             @Override
             public String getTitle(int position) {
+                return mMainTitle.get(position);
+            }
+
+            @Override
+            public String getSubTitle(int position) {
+                if(position ==0)
+                    return  mSubTitle.get(0);
                 return null;
+            }
+        });
+        mButtonTry=mOpeningLooperView.getButtonView();
+        mButtonTry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Try it now",Toast.LENGTH_SHORT).show();
+                return;
             }
         });
     }
@@ -69,5 +81,14 @@ public class MainActivity extends Activity {
         imageView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
         imageView.setImageResource(R.drawable.tutorial_5_16to9);
         mList.add(imageView);
+
+        mMainTitle = new ArrayList<>();
+        mMainTitle.add("Motion Graphics Titles");
+        mMainTitle.add("Experience the freedom of creation");
+        mMainTitle.add("Fine-tune your aesthetic");
+        mMainTitle.add("Inspire your Imagination");
+        mMainTitle.add("Having fun exploring a vast stock library");
+        mSubTitle = new ArrayList<>();
+        mSubTitle.add("Galvanize your videos with cinematic titles");
     }
 }
--- LooperView/app/src/main/java/com/example/looperview/OpeningLooperView.java
+++ LooperView/app/src/main/java/com/example/looperview/OpeningLooperView.java
@@ -5,6 +5,7 @@ import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -23,26 +24,28 @@ public class OpeningLooperView extends LinearLayout {
     private TextView mSubtitle;
     private TextView mMainTitle;
     private ViewPager mViewPager;
+    private Button mButtonTry;
     private TitleBindListener mTitleBindListener;
-    private int mOldSelectedPosition=0;
+    private int mOldSelectedPosition = 0;
+    private ViewPager.OnPageChangeListener onPageChangeListener;
 
     public OpeningLooperView(Context context) {
-        this(context,null);
+        this(context, null);
     }
 
     public OpeningLooperView(Context context, @Nullable AttributeSet attrs) {
-        this(context, attrs,0);
+        this(context, attrs, 0);
     }
 
     public OpeningLooperView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        LayoutInflater.from(getContext()).inflate(R.layout.layout_opening_looper_view,this,true);
+        LayoutInflater.from(getContext()).inflate(R.layout.layout_opening_looper_view, this, true);
         initView();
         initScrollEvent();
     }
 
     private void initScrollEvent() {
-        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+        onPageChangeListener = new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
@@ -51,70 +54,118 @@ public class OpeningLooperView extends LinearLayout {
             @Override
             public void onPageSelected(int position) {
                 updatePointIndicator(position);
+                String mainTitle = mTitleBindListener.getTitle(position);
+                String subTitle = mTitleBindListener.getSubTitle(position);
+                //only the 1st view page should display 'TryItNow' Button
+                if (position == 0) {
+                    mButtonTry.setVisibility(VISIBLE);
+                } else {
+                    mButtonTry.setVisibility(INVISIBLE);
+                }
+                //dynamically adjust heights of 2 kind of titles inorder to fit UI's design
+                if (mainTitle != null)
+                    mMainTitle.setText(mainTitle);
+                LayoutParams mainTitleLayoutParams = ((LayoutParams) mMainTitle.getLayoutParams());
+                LayoutParams subTitleLayoutParams = ((LayoutParams) mSubtitle.getLayoutParams());
+                if (subTitle != null) {
+                    mainTitleLayoutParams.weight = 0.4f;
+                    subTitleLayoutParams.weight = 0.6f;
+                    mMainTitle.setLayoutParams(mainTitleLayoutParams);
+                    mSubtitle.setLayoutParams(subTitleLayoutParams);
+                    mSubtitle.setText(subTitle);
+                } else {
+                    mainTitleLayoutParams.weight = 0.6f;
+                    subTitleLayoutParams.weight = 0.4f;
+                    mMainTitle.setLayoutParams(mainTitleLayoutParams);
+                    mSubtitle.setLayoutParams(subTitleLayoutParams);
+                    mSubtitle.setText("");
+                }
             }
 
             @Override
             public void onPageScrollStateChanged(int state) {
 
             }
-        });
+        };
+        mViewPager.addOnPageChangeListener(onPageChangeListener);
     }
 
     private void initView() {
         mViewPager = findViewById(R.id.intro_pager);
         mMainTitle = findViewById(R.id.intro_mainTitle);
         mSubtitle = findViewById(R.id.intro_subtitle);
+        mButtonTry = findViewById(R.id.btn_try);
+        mButtonTry.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
         //point Container, points need to be created dynamically based on the number of views in view pager
         mPointContainer = findViewById(R.id.intro_point_container);
 
     }
+
+    /**
+     * developer can customize the behavior of the try it Button
+     */
+    public Button getButtonView() {
+        return mButtonTry;
+    }
+
     /**
      * user can define title outside the class
-     * */
+     */
     public interface TitleBindListener {
         String getTitle(int position);
+
+        String getSubTitle(int position);
     }
 
-    /**update the bottom points when the selected page is changed
-     * */
-    public void updatePointIndicator(int position){
-        mPointContainer.getChildAt(mOldSelectedPosition).setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.dot_n,null));
-        mPointContainer.getChildAt(position).setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.dot_s,null));
-        mOldSelectedPosition=position;
+    /**
+     * update the bottom points when a page is selected
+     */
+    public void updatePointIndicator(int position) {
+        mPointContainer.getChildAt(mOldSelectedPosition).setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.dot_n, null));
+        mPointContainer.getChildAt(position).setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.dot_s, null));
+        mOldSelectedPosition = position;
 
     }
-    public void initPointIndicator(InnerPageAdapter innerPageAdapter){
-        for(int i=0;i<innerPageAdapter.getDataSize();i++){
+
+    /**
+     * create the corresponding number of point indicators that below videos
+     */
+    public void initPointIndicator(InnerPageAdapter innerPageAdapter) {
+        for (int i = 0; i < innerPageAdapter.getCount(); i++) {
             View point = new View(getContext());
-            LayoutParams layoutParams= new LayoutParams(SizeUtils.dip2px(getContext(),10),SizeUtils.dip2px(getContext(),10));
-            layoutParams.setMargins(SizeUtils.dip2px(getContext(),5),0,SizeUtils.dip2px(getContext(),5),0);
+            LayoutParams layoutParams = new LayoutParams(SizeUtils.dip2px(getContext(), 6.7f), SizeUtils.dip2px(getContext(), 6.7f));
+            layoutParams.setMargins(SizeUtils.dip2px(getContext(), 3.65f), 0, SizeUtils.dip2px(getContext(), 3.65f), 0);
             point.setLayoutParams(layoutParams);
-
-            point.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.dot_n,null));
-
+            point.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.dot_n, null));
             mPointContainer.addView(point);
         }
-        updatePointIndicator(0);
     }
-    public void setData(InnerPageAdapter innerPageAdapter,TitleBindListener listener) {
+
+    public void setData(InnerPageAdapter innerPageAdapter, TitleBindListener listener) {
         mViewPager.setAdapter(innerPageAdapter);
         this.mTitleBindListener = listener;
         initPointIndicator(innerPageAdapter);
+        //select 1st view of the viewPager so that the title and the point indicator can be selected.
+        onPageChangeListener.onPageSelected(0);
     }
+
     public static abstract class InnerPageAdapter extends PagerAdapter {
-        protected  final List<ImageView> videos;
 
-        protected InnerPageAdapter(List<ImageView> videos) {
-            this.videos = videos;
+        public int viewsCount;// number of view to be loaded
+
+        protected InnerPageAdapter(int count) {
+            viewsCount = count;
         }
-
-        public abstract int getDataSize();
 
         @Override
         public int getCount() {
             //viewpager的數量就是videos外面传进来的list大小
-            return videos.size();
+            return viewsCount;
         }
 
         @Override
@@ -126,17 +177,17 @@ public class OpeningLooperView extends LinearLayout {
         @Override
         public Object instantiateItem(@NonNull ViewGroup container, int position) {
             //载入view，至于显示什么view,不用管，由外面给进来。只要对position进行一个转换
-            View itemView = videos.get(position);
+            View itemView = getItemView(container, position);
             container.addView(itemView);
             return itemView;
         }
 
         //至于要什么view，我不管，由外面给我。
-        protected abstract View getItemView(ViewGroup container,int itemPosition);
+        protected abstract View getItemView(ViewGroup container, int itemPosition);
 
         @Override
-        public void destroyItem(@NonNull ViewGroup container,int position,@NonNull Object object) {
-            container.removeView(videos.get(position));
+        public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+            container.removeView((View) object);
         }
     }
 }
